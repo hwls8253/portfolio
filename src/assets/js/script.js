47d4049eab2ba7266f86cc957d59ffa01e02d0ba
DESKTOP-J4RKVCP\김혜진 <khwls8253@gmail.com>
2019-12-26 19:16:00 +0900

    menu page 작업

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,9 @@
 function menu(){
   $('.menu-btn').toggleClass('close-btn');
+  $('.menu-wrap').toggleClass('on');
+  $('.menu-wrap nav li').hover(function(){
+    $(this).addClass('hover').siblings('li').removeClass('hover');
+  });
 }
 // $(function(){
 // })
